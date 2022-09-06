266e75e6f5516e0742b10b3d900c32f5dfa19d40
GitHub Action <action@github.com>
2022-09-06 01:23:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/leemuyi/redistool/MyBundle.kt
+++ src/main/kotlin/com/github/leemuyi/redistool/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.leemuyi.redistool
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/leemuyi/redistool/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/leemuyi/redistool/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.leemuyi.redistool.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leemuyi.redistool.MyBundle
 
 class MyApplicationService {
 
